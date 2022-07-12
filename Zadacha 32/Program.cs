08a860d09b10ac5c3f1949b574ae95f7fbb53e27
Azat Minimullin <minimullin.azat@yandex.ru>
2022-07-12 16:30:00 +0300

    add Zadacha 32

---
+++ Zadacha 32/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задача 32: Напишите программу замена элементов массива: 
+положительные элементы замените на соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2]  */
+Console.Clear();
+int[] array = new int[4] { -4, -8, 8, 2 };
+void ChangeSignArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+}
+ChangeSignArray(array);
+Console.WriteLine(string.Join(", ", array));
