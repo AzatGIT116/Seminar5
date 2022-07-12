ec3a508e07fe52663fc3b510150bb641c7aa9aa6
Azat Minimullin <minimullin.azat@yandex.ru>
2022-07-12 16:30:49 +0300

    add Zadacha 33

---
+++ Zadacha33/Program.cs
@@ -0,0 +1,18 @@
+﻿/*  Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+4; массив [6, 7, 19, 345, 3] -> нет
+-3; массив [6, 7, 19, 345, 3] -> да */
+
+int[] ourArray = { 5, -5, 6, 4 };
+CheckIfNumberinArray(ourArray, 4);
+void CheckIfNumberinArray(int[] array, int number)
+{
+    foreach (var item in array)
+    {
+        if (item == number)
+        {
+            Console.WriteLine($"The number {number} is in thes array");
+            return;
+        }
+    }
+    Console.WriteLine($"the number {number} is NOT in this array");
+}
